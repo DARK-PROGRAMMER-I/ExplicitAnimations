9dfd2587f84b061b8378ef8002f395098a118821
DARK-PROGRAMMER-I <hammadmalli64@gmail.com>
2023-01-29 23:02:21 +0500

    init

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,10 +1,35 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateMixin{
+  late AnimationController _animationController;
+  late Animation _animation;
+  
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 2)
+    );
+
+    _animation = Tween(begin: 0, end: 2 * pi).animate(_animationController);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        
+      ),
+    );
   }
 }
